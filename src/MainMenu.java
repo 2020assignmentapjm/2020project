00e9f65c1b00ec5af9abface616de2785f2a3d7b
2020assignmentapjm <47828077+2020assignmentapjm@users.noreply.github.com>
2019-03-04 16:07:08 -0500

    Add files via upload
    Updated Menu with Play, Credits, and Exit functional

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,63 +1,54 @@
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import javafx.animation.FadeTransition;
-import javafx.animation.ParallelTransition;
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.effect.GaussianBlur;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
-public class MainMenu extends Application {
-
-    private static final Font FONT = Font.font("", FontWeight.BOLD, 18);
-
-    private VBox menuBox;
-    private VBox menuBox2;
-    private VBox menuBox3;
-    private int currentItem = 0;
-
-    private int messages = 0;
-
-    private ScheduledExecutorService bgThread = Executors.newSingleThreadScheduledExecutor();
-
-    private Parent createContent() {
-        Pane root = new Pane();
+public class MainMenu extends Application 
+{
+    private static VBox menuBox;
+    private static VBox menuBox2;
+    private static VBox menuBox3;
+    static Rectangle bg;
+    private static int currentItem = 0;
+    static Scene scene;
+    
+    private static MenuItem getMenuItem(int index) 
+    {
+        return (MenuItem)menuBox.getChildren().get(index);
+    }
+    
+    public static Parent createContent(Stage primaryStage)
+    {
+    	
+    	Pane root = new Pane();
         root.setPrefSize(900, 600);
-
-        Rectangle bg = new Rectangle(900, 600);
-
-
-        
+    	bg = new Rectangle(900, 600);
+        sceneCredit sceneCredit = new sceneCredit();
+        scenePlay scenePlay = new scenePlay();
         MenuItem itemExit = new MenuItem("EXIT");
+        MenuItem itemCredits = new MenuItem("CREDITS");
+        MenuItem itemPlay = new MenuItem("PLAY");
         itemExit.setOnActivate(() -> System.exit(0));
-
+        itemCredits.setOnActivate(()-> 
+        {
+        	primaryStage.setScene(sceneCredit.getScene());
+        	sceneCredit.setStage(primaryStage);
+        });
+        itemPlay.setOnActivate(()->
+        {
+        	primaryStage.setScene(scenePlay.getScene());
+        	scenePlay.setStage(primaryStage);
+        });
         menuBox = new VBox(10,
-        		new MenuItem("PLAY"),
-                new MenuItem("CREATE SERVER"),
-                new MenuItem("JOIN SERVER"),
+        		itemPlay,
                 new MenuItem("OPTIONS"),
-                new MenuItem("CREDITS"),
+                itemCredits,
                 itemExit);
         menuBox.setAlignment(Pos.TOP_CENTER);
         menuBox.setTranslateX(360);
@@ -65,148 +56,62 @@ public class MainMenu extends Application {
         
         menuBox2 = new VBox(10, new MenuItem("CSCI2020U PROJECT"));
         menuBox2.setAlignment(Pos.TOP_CENTER);
-        menuBox2.setTranslateX(340);
+        menuBox2.setTranslateX(317);
         menuBox2.setTranslateY(100);
         
         menuBox3 = new VBox(10, new MenuItem("TEXAS HOLD-EM"));
         menuBox3.setAlignment(Pos.TOP_CENTER);
-        menuBox3.setTranslateX(355);
+        menuBox3.setTranslateX(332);
         menuBox3.setTranslateY(140);
 
 
         getMenuItem(0).setActive(true);
         root.getChildren().addAll(bg,menuBox, menuBox2, menuBox3);
         return root;
-    }
-    
-    private Node createLeftContent() {
-        final Text inbox = new Text("");
-        inbox.setFill(Color.WHITE);
-
         
-
-        return inbox;
     }
-
-
-    private Node createMiddleContent() {
-        String title = "CSCI2020U Project";
-        HBox letters = new HBox(0);
-        letters.setAlignment(Pos.CENTER);
-        for (int i = 0; i < title.length(); i++) {
-            Text letter = new Text(title.charAt(i) + "");
-            letter.setFont(FONT);
-            letter.setFill(Color.WHITE);
-            letters.getChildren().add(letter);
-        }
-
-        return letters;
-    }
-
-
-    private MenuItem getMenuItem(int index) {
-        return (MenuItem)menuBox.getChildren().get(index);
-    }
-
-    private static class ContentFrame extends StackPane {
-        public ContentFrame(Node content) {
-            setAlignment(Pos.CENTER);
-
-            Rectangle frame = new Rectangle(200, 200);
-            frame.setArcWidth(25);
-            frame.setArcHeight(25);
-            frame.setStroke(Color.WHITESMOKE);
-
-            getChildren().addAll(frame, content);
-        }
-    }
-
-    private static class MenuItem extends HBox {
-        private TriCircle c1 = new TriCircle(), c2 = new TriCircle();
-        private Text text;
-        private Runnable script;
-
-        public MenuItem(String name) {
-            super(15);
-            setAlignment(Pos.CENTER);
-
-            text = new Text(name);
-            text.setFont(FONT);
-            text.setEffect(new GaussianBlur(2));
-
-            getChildren().addAll(c1, text, c2);
-            setActive(false);
-            setOnActivate(() -> System.out.println(name + " activated"));
-        }
-
-        public void setActive(boolean b) {
-            c1.setVisible(b);
-            c2.setVisible(b);
-            text.setFill(b ? Color.WHITE : Color.GREY);
-        }
-
-        public void setOnActivate(Runnable r) {
-            script = r;
-        }
-
-        public void activate() {
-            if (script != null)
-                script.run();
-        }
-    }
-
-    private static class TriCircle extends Parent {
-        public TriCircle() {
-            Shape shape1 = Shape.subtract(new Circle(5), new Circle(2));
-            shape1.setFill(Color.WHITE);
-
-            Shape shape2 = Shape.subtract(new Circle(5), new Circle(2));
-            shape2.setFill(Color.WHITE);
-            shape2.setTranslateX(5);
-
-            Shape shape3 = Shape.subtract(new Circle(5), new Circle(2));
-            shape3.setFill(Color.WHITE);
-            shape3.setTranslateX(2.5);
-            shape3.setTranslateY(-5);
-
-            getChildren().addAll(shape1, shape2, shape3);
-
-            setEffect(new GaussianBlur(2));
-        }
-    }
-
     
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Scene scene = new Scene(createContent());
+    public void start(Stage primaryStage) throws Exception 
+    {
+        scene = new Scene(createContent(primaryStage));
+        
         scene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.UP) {
-                if (currentItem > 0) {
+            if (event.getCode() == KeyCode.UP) 
+            {
+                if (currentItem > 0) 
+                {
                     getMenuItem(currentItem).setActive(false);
                     getMenuItem(--currentItem).setActive(true);
                 }
             }
 
-            if (event.getCode() == KeyCode.DOWN) {
-                if (currentItem < menuBox.getChildren().size() - 1) {
+            if (event.getCode() == KeyCode.DOWN) 
+            {
+                if (currentItem < menuBox.getChildren().size() - 1) 
+                {
                     getMenuItem(currentItem).setActive(false);
                     getMenuItem(++currentItem).setActive(true);
                 }
             }
 
-            if (event.getCode() == KeyCode.ENTER) {
+            if (event.getCode() == KeyCode.ENTER) 
+            {
                 getMenuItem(currentItem).activate();
             }
         });
 
+        
         primaryStage.setScene(scene);
-        primaryStage.setOnCloseRequest(event -> {
-            bgThread.shutdownNow();
-        });
         primaryStage.show();
     }
-
-    public static void main(String[] args) {
+    
+    public static Scene getScene()
+	{
+		return scene;
+	}
+    public static void main(String[] args) 
+    {
         launch(args);
     }
 }
---
+++ src/TriCircle.java
@@ -0,0 +1,27 @@
+import javafx.scene.Parent;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
+
+public class TriCircle extends Parent 
+    {
+        public TriCircle() 
+        {
+            Shape shape1 = Shape.subtract(new Circle(5), new Circle(2));
+            shape1.setFill(Color.WHITE);
+
+            Shape shape2 = Shape.subtract(new Circle(5), new Circle(2));
+            shape2.setFill(Color.WHITE);
+            shape2.setTranslateX(5);
+
+            Shape shape3 = Shape.subtract(new Circle(5), new Circle(2));
+            shape3.setFill(Color.WHITE);
+            shape3.setTranslateX(2.5);
+            shape3.setTranslateY(-5);
+
+            getChildren().addAll(shape1, shape2, shape3);
+
+            setEffect(new GaussianBlur(2));
+        }
+    }
